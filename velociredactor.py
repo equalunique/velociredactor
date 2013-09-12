e8b1e75b3f9ca532759457ce566864e8c0fcad2a
Evan Rowley <evan.rowley@gmail.com>
2013-09-11 21:57:38 -0400

    Messy single PDF to PNGs to PDFs to single PDF

--- velociredactor.py
+++ velociredactor.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python
+import os
+import tempfile
 import argparse
+import subprocess
 
 parser = argparse.ArgumentParser(description='Redact info from PDF based on GIMP template')
 #parser.add_argument('ipdf', required=True, help='PDF to perform redaction on')
@@ -12,6 +15,173 @@ parser.add_argument('--osfx', help='Appendied suffix for redacted output PDF', r
 parser.add_argument('--tmpl', help='GIMP template for redaction', required=True)
 args = parser.parse_args()
 
-print("ipdf: %s" % args.ipdf )
-print("osfx: %s" % args.osfx )
-print("tmpl: %s" % args.tmpl )
+
+#print("ipdf_abs: %s" % os.path.abspath(args.ipdf))
+ipdf_abs = os.path.abspath(args.ipdf)
+ipdf_bas = os.path.basename(ipdf_abs)
+ipdf_dir = os.path.dirname(ipdf_abs)
+if os.path.isfile(ipdf_abs):
+    print("ipdf_abs: %s" % ipdf_abs)
+    print("ipdf: %s" % os.path.basename(ipdf_abs))
+else:
+    print("invalid argument for ipdf: %s" % args.ipdf )
+
+#print("tmpl_abs: %s" % os.path.abspath(args.tmpl))
+tmpl_abs = os.path.abspath(args.tmpl)
+tmpl_bas = os.path.basename(tmpl_abs)
+if os.path.isfile(tmpl_abs):
+    print("tmpl_abs: %s" % tmpl_abs)
+    print("tmpl: %s" % os.path.basename(tmpl_abs))
+else:
+    print("invalid argument for tmpl: %s" % args.tmpl )
+
+print("osfx: %s" % args.osfx)
+
+nosp_ipdf_bas = ipdf_bas
+nosp_ipdf_bas = nosp_ipdf_bas.replace(" ", "_")
+nosp_ipdf_bas = nosp_ipdf_bas.replace(",","")
+nosp_ipdf_bas = nosp_ipdf_bas.replace("(","")
+nosp_ipdf_bas = nosp_ipdf_bas.replace(")","")
+print("nosp_ipdf_bas: %s" % nosp_ipdf_bas)
+
+nosp_ipdf_abs = ipdf_abs.replace(ipdf_bas, nosp_ipdf_bas)
+print("nosp_ipdf_abs: %s" % nosp_ipdf_abs)
+os.rename(ipdf_abs, nosp_ipdf_abs)
+
+fileName, fileExtension = os.path.splitext(nosp_ipdf_bas)
+print("fileName: %s" % fileName)
+print("fileExtension: %s" % fileExtension)
+print("gs thingy: %d")
+tup = (fileName, "%d", "png")
+pngt_bas = '.'.join(tup)
+
+# Create Temp Directory
+tdir = tempfile.mkdtemp()
+#print tdir
+
+# PDF 2 PNGs via GhostScript
+#output = Popen(["mycmd", "myarg"], stdout=PIPE).communicate()[0]
+
+#output = Popen([
+#   'gs',
+#   '-q',
+#   '-dNOPAUSE',
+#   '-dBATCH',
+#   '-sDEVICE=pngalpha',
+#   '-r300',
+#   '-dEPSCrop',
+#   '-sOutputFile="%s %s"' % (pngt_bas, nosp_ipdf_abs),
+#], stdout=PIPE).communicate()[0]
+
+os.chdir(tdir)
+
+print("pngt_bas: %s" % pngt_bas)
+print("nosp_ipdf_abs: %s" % nosp_ipdf_abs)
+sub = ''.join([
+	'gs'
+	' -q',
+	' -dNOPAUSE',
+	' -dBATCH',
+	' -sDEVICE=pngalpha',
+	' -r300',
+	' -dEPSCrop',
+	' -sOutputFile=',
+	pngt_bas,
+	' ',
+	nosp_ipdf_abs
+])
+print(sub)
+subprocess.check_output([sub], shell=True)
+
+
+
+
+#output = Popen([
+#    self.tdir, 
+#   'inkscape',
+#   '%s' % (ipdf_abs)),
+#   '-z',
+#   '--export-dpi=600',
+#   '--export-area-drawing',
+#   '--export-png="%s"' % (args.ipdf+args.osfx),
+#], stdout=PIPE).communicate()[0]
+
+
+
+print("Opened temporary directory: %s" % tdir)
+png_file_list = sorted(os.listdir(tdir))
+for png_file in png_file_list:
+	print("Created %s" % png_file)
+
+
+# For each PNG
+
+	# Load PNG to libgimp
+
+	# Deskew PNGs via libgimp and deskew
+
+	# Apply Redaction Template via libgimp
+
+	# Save as Redacted PNG
+
+# PNGs 2 PDFs via ImageMagick
+i = 1
+pdf_file_list = []
+for png_file in png_file_list:
+	pdf_file = '.'.join([
+		fileName,
+		str(i),
+		'pdf'
+	])
+	sub = ''.join([
+		'/usr/bin/convert ',
+		png_file,
+		' ',
+		pdf_file
+	])
+	i = i + 1
+	pdf_file_list.append(pdf_file)
+	print(sub)
+	subprocess.check_output([sub], shell=True)
+
+
+
+
+# Consolidate PDFs via GhostScript
+
+fileName, fileExtension = os.path.splitext(nosp_ipdf_bas)
+opdf_bas = '.'.join([fileName, "redacted", 'pdf'])
+opdf_abs = '/'.join([ipdf_dir, opdf_bas])
+sub = ''.join([
+	''.join([
+		'gs'
+		' -q',
+		' -dNOPAUSE',
+		' -dBATCH',
+		' -dSAFER',
+		' -sDEVICE=pdfwrite',
+		' -sOutputFile=',
+		opdf_abs,
+		' ',
+	]),
+	' '.join(
+		pdf_file_list
+	)
+])
+print(sub)
+subprocess.check_output([sub], shell=True)
+
+
+# Clear Temp Directory
+print("Cleaning temporary files in %s" % tdir)
+for the_file in os.listdir(tdir):
+	file_path = os.path.join(tdir, the_file)
+	try:
+		os.unlink(file_path)
+		print("Deleted temporary file %s" % the_file)
+	except Exception, e:
+		print e
+print("File deletions complete. Removing temporary directory.")
+# Close Temp Directory
+os.removedirs(tdir)
+print("Deleted %s" % tdir)
